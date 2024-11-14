65fbad3c2bb47b2a4af26d5f55c78018d2c99e76
raman-mapsense <210108077@HBTU.AC.IN>
2024-11-15 02:06:08 +0530

    Config the backend

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => console.error('MongoDB connection error:', err));
